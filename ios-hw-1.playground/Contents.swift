bf1db903e6b55f0b8a52ee23acac8e687ebe3fc9
fatema-mohammad-alenezi-472 <66527004+fatema-mohammad-alenezi-472@users.noreply.github.com>
2020-06-14 18:15:53 +0300

    homework 1 done
    thank you!

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,10 +7,10 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let Name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,36 +38,130 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "asmaa"
+var lastName1 = "alenezi"
+var email1 = "asmaanasser09@hotmail.com"
+var phoneNumber1 = 98756420
+var age1 = 16
+var country1 = "kuwait"
+var area1 = "adan"
+var password1 = "12345678asmaa"
+var isKuwaiti1 = true
+
 
 //Person 2
-
-
-
-
-
-
-
-
+var firstname2 = "abullah"
+var lastname2 = "alenezi"
+var email2 = "abullahisthebest@hotmail.com"
+var phonenumber2 = 77865498
+var age2 = 10
+var country2 = "saudi arabia"
+var area2 = "fintais"
+var password2 = "abdullahisthebest32"
+var iskuwaiti2 = false
 
 
 //Person 3
+var firstname3 = "sarah"
+var lastname3 = "alenezi"
+var email3 = "eng.sarah@hotmail.com"
+var phonenumber3 = 50567482
+var age3 = 18
+var country3 = " kuwait"
+var area3 = "adan"
+var pasword3 = "eng.sarah234"
+var iskuwaiti3 = true
+
+// the comparison
+firstName1 == firstname2
+firstName1 != firstname2
+firstname2 == firstname3
+firstname2 != firstname3
+firstname3 == firstName1
+firstname3 != firstName1
+
+lastName1 == lastname2
+lastName1 != lastname2
+lastname2 == lastname3
+lastname2 != lastname3
+lastname3 == lastName1
+lastname3 != lastName1
 
 
+email1 == email2
+email1 != email2
+email2 == email3
+email2 != email3
+email3 == email1
+email3 != email1
 
 
+phoneNumber1 > phonenumber2
+phoneNumber1 < phonenumber2
+phoneNumber1 >= phonenumber2
+phoneNumber1 <= phonenumber2
+phoneNumber1 == phonenumber2
+phoneNumber1 != phonenumber2
+phonenumber2 > phonenumber3
+phonenumber2 < phonenumber3
+phonenumber2 >= phonenumber3
+phonenumber2 <= phonenumber3
+phonenumber2 == phonenumber3
+phonenumber2 != phonenumber3
+phonenumber3 > phoneNumber1
+phonenumber3 < phoneNumber1
+phonenumber3 >= phoneNumber1
+phonenumber3 <= phoneNumber1
+phonenumber3 == phoneNumber1
+phonenumber3 != phoneNumber1
 
 
+age1 > age2
+age1 < age2
+age1 >= age2
+age1 <= age2
+age1 == age2
+age1 != age2
+age2 > age3
+age2 < age3
+age2 >= age3
+age2 <= age3
+age2 == age3
+age2 != age3
+age1 > age3
+age1 < age3
+age1 >= age3
+age1 <= age3
+age1 == age3
+age1 != age3
+age1 < 18
+age1 > 18
+age1 == 17
+age1 != 17
+age2 < 18
+age2 > 18
+age2 == 17
+age2 != 17
+age3 < 18
+age3 > 18
+age3 == 17
+age3 != 17
 
 
+area1 == area2
+area1 != area2
+area2 == area3
+area2 != area3
+area3 == area1
+area3 != area1
+
+
+country1 == country2
+country1 != country2
+country2 == country3
+country2 != country3
+country3 == country1
+country3 != country1
 
 /*
 - - - -
@@ -81,4 +175,4 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
  
- */
+ /**/*/
